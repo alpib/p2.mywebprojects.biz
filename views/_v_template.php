cdb5d0bbd1fbb945d8d8a65967b9208e95f97c08
Alpana Barua <alpanabarua@g.harvard.edu>
2013-10-30 16:25:35 -0400

    Add a master css file
    Master css file added.  not complete.  master css is included in the
    master template  _v_template.php

--- views/_v_template.php
+++ views/_v_template.php
@@ -4,13 +4,38 @@
 	<title><?php if(isset($title)) echo $title; ?></title>
 
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-
+    <!-- Common JS/CSS -->
+    <link rel=stylesheet type="text/css" href="/css/master.css">
 	<!-- Controller Specific JS/CSS -->
 	<?php if(isset($client_files_head)) echo $client_files_head; ?>
 	
 </head>
 
 <body>	
+	<div id="container">
+    <div id="header">
+    <div id='navbar'>
+        <ul id="navbar">
+        <li><a href='/'>Home</a></li>
+
+        <!-- Menu for users who are logged in -->
+        <?php if($user): ?>
+            
+            <li><a href='/users/logout'>Logout</a></li>
+            <li><a href='/users/profile'>Profile</a></li>
+            
+
+        <!-- Menu options for users who are not logged in -->
+        <?php else: ?>
+
+            <li><a href='/users/signup'>Sign up</a></li>
+            <li><a href='/users/login'>Log in</a></li>
+
+        <?php endif; ?>
+        </ul>
+    </div>
+
+    <br>
 
 	<?php if(isset($content)) echo $content; ?>
 
--- views/v_users_login.php
+++ views/v_users_login.php
@@ -1,5 +1,12 @@
 <form method='POST' action='/users/p_login'>
 
+     <?php if(isset($error)): ?>
+        <div class='error'>
+            Login failed. Please double check your email and password.
+        </div>
+        <br>
+    <?php endif; ?>
+    
     Email<br>
     <input type='text' name='email'>
 
